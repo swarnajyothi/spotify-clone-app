4a4f1bf1e443265ebde7f2677b7b16f422be270c
swarna <Dangeti.JayaSwarnaJyothi@icims.com>
2021-07-15 23:37:14 +0530

    clear code

---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css';
+function Login() {
+    return (
+        <div>
+            {/*Spotify Logo */}
+            {/*Login button*/}
+            <h1>Iam the login page</h1>
+        </div>
+    )
+}
+
+export default Login
